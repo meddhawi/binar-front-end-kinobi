63955460d429b5e7cc8f44eea1b8b04ac9f06638
meddhawi <meddhawi@gmail.com>
2022-05-12 11:48:10 +0700

    added redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import withReduxStore from '../utils/withReduxStore'
+import {Provider} from 'react-redux'
+
+class MyApp extends App {
+  constructor(props){
+    super(props)
+  }
+  
+  componentDidMount = () => {}
+
+  render(){
+    const { Component, pageProps, reduxStore} = this.props
+
+    return(
+      <Provider store={reduxStore}>
+        <Component {...pageProps} />
+      </Provider>
+    )
+  }
 }
 
-export default MyApp
+export default withReduxStore(MyApp)
